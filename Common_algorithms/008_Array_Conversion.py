8f5107ae89158e8bb1140d5526f08b262c118744
zhangkun <zhangkun09211916@gmail.com>
2019-03-07 18:00:26 +0800

    Selection Sort

---
+++ Common_algorithms/008_Array_Conversion.py
@@ -0,0 +1,34 @@
+"""
+    数组[[1,2],[3,4]] 转换成[1,2,3,4]
+
+def func(alist):
+    result = []
+    for i in range(2):
+        for j in range(2):
+            print(alist[i][j])
+            result.append(alist[i][j])
+    return result
+
+
+print(func([[1,2],[3,4]]))
+
+"""
+
+a = [[1,2],[3,4]]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
