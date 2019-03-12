cd84bd6023c75849eeed5e5ef29eb5237bf668c0
zhangkun <zhangkun09211916@gmail.com>
2019-03-12 21:25:58 +0800

    remove duplicates

--- Easy/026_remove_Duplicates.py
+++ Easy/026_remove_Duplicates.py
@@ -2,20 +2,27 @@
 
 class Solution:
     def revoveDuplicates(self, nums):
+        if len(nums) == 0:
+            return 0
 
-        temp_list = []
-        for key,num in enumerate(nums):
-            if num not in temp_list:
-                temp_list.append(num)
+        slowIndex = 0
+        quickIndex = 1
 
-        print(temp_list)
-        return len(temp_list)
+        bound = len(nums)-1
+        while quickIndex <= bound:
+            if nums[slowIndex] != nums[quickIndex]:
+                slowIndex += 1
+                nums[slowIndex] = nums[quickIndex]
 
+            quickIndex += 1
+
+        print(nums)
+        return slowIndex + 1
 
 
 if __name__ == "__main__":
 
     s = Solution()
-    alist = [1,1,2]
+    alist = [0,0,1,1,1,2,2,2,3,3,4]
     print(s.revoveDuplicates(alist))
 
