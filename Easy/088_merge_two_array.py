0a973c927d550f342508e46aa455e74b21427d18
zhangkun <zhangkun09211916@gmail.com>
2019-03-15 10:46:13 +0800

    merge

--- Easy/088_merge_two_array.py
+++ Easy/088_merge_two_array.py
@@ -15,18 +15,6 @@
 
 """
 
-class Solution:
-    def merge(self, nums1, m, nums2, n):
-
-
-if __name__ == "__main__":
-
-    num1 = [1, 2, 3, 7, 8]
-    num2 = [2, 5, 6, 9]
-
-    S = Solution()
-    aaa=S.merge(num1, 5, num2, 4)
-    print(aaa)
 
 
 
