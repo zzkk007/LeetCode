c057e70f91ca1937c63db8424743118d6faf2bb9
zzkk007 <38235797+zzkk007@users.noreply.github.com>
2019-03-15 23:53:07 +0800

    merge

--- Easy/088_merge_two_array.py
+++ Easy/088_merge_two_array.py
@@ -16,6 +16,29 @@
 """
 
 
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+
+        while m > 0 and n > 0:
+            if nums1[m - 1] > nums2[n - 1]:
+                nums1[m + n - 1] = nums1[m - 1]
+                m -= 1
+            else:
+                nums1[m + n - 1] = nums2[n - 1]
+                n -= 1
+
+        if n > 0:
+            nums1[:n] = nums2[:n]
+        print(nums1)
+
+if __name__ == "__main__":
+
+    S = Solution()
+    nums1 = [4, 5, 6, 0, 0, 0]
+    nums2 = [1, 2, 3]
+
+    S.merge(nums1, 3, nums2, 3)
+
 
 
 
