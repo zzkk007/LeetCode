da012e8c89fb8d94644e96f6c67b02fb95ae4ffe
zhangkun <zhangkun09211916@gmail.com>
2019-03-15 10:41:39 +0800

    merge

--- Easy/088_merge_two_array.py
+++ Easy/088_merge_two_array.py
@@ -17,33 +17,15 @@
 
 class Solution:
     def merge(self, nums1, m, nums2, n):
-        """
-        Do not return anything, modify nums1 in-place instead.
-        """
-        l, r = 0, 0
-        result = []
-
-        while l < m and r < n:
-
-            if nums1[l] < nums2[r]:
-                result.append(nums1[l])
-                l += 1
-            else:
-                result.append(nums2[r])
-                r += 1
-
-        result += nums1[l:]
-        result += nums2[r:]
-        return result
 
 
 if __name__ == "__main__":
 
-    num1 = [1,2,3,4]
-    num2 = [2,3,5,6,7]
+    num1 = [1, 2, 3, 7, 8]
+    num2 = [2, 5, 6, 9]
 
     S = Solution()
-    aaa=S.merge(num1,4,num2,5)
+    aaa=S.merge(num1, 5, num2, 4)
     print(aaa)
 
 
