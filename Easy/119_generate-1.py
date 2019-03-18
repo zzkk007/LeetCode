a739c206a869460b194be7d596f0dcf8fd08f755
zzkk007 <38235797+zzkk007@users.noreply.github.com>
2019-03-18 23:33:23 +0800

    generate

--- Easy/119_generate-1.py
+++ Easy/119_generate-1.py
@@ -9,3 +9,31 @@
 
 """
 
+
+
+class Solution(object):
+
+    def getRow(self, rowIndex):
+        line = [1]
+        for s in range(rowIndex + 1):
+            next_line = [1] * (s + 1)
+            print('--------')
+            print(next_line)
+            for i in range(len(line)):
+                if i == 0:
+                    continue
+                next_line[i] = line[i] + line[i-1]
+            line = next_line
+            print(line)
+            print('---------')
+
+        return line
+
+
+
+if __name__ == "__main__":
+
+
+    S = Solution()
+    print(S.getRow(3))
+
