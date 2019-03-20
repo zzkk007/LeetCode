1ff4efdba2440656150c9bb1751321017653635e
zhangkun <zhangkun09211916@gmail.com>
2019-03-20 11:27:38 +0800

    maxProfit

---
+++ Easy/167_twoSum.py
@@ -0,0 +1,39 @@
+"""
+
+    给定一个已按照升序排序的有序数组，找到两个数使得它们相加之和等于目标数。
+    函数应该返回这两个下标值 index1 和 index2, 其中 index1 必须小于 index2。
+
+    说明：
+
+        返回下标(index1 和 index2)不是从零开始的。
+        你可以假设每个输入值对应唯一的答案，而且你不可以重复使用相同的元素。
+
+    示例：
+
+        输入：numbers = [2, 7, 11, 15], target = 9
+        输出：[1, 2]
+        解释：2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+
+"""
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        pass
+
+
+if __name__ == "__main__":
+    S = Solution()
+    S.twoSum([1,2,3,4,5,6,7,8], 10)
+
+
+
+
+
+
+
+
+
+
+
+
+
