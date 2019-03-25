47833486a974590e9d889392e9fd2a7b9378972c
zhangkun <zhangkun09211916@gmail.com>
2019-03-25 14:12:19 +0800

    move zeroes

--- Easy/283_moveZeroes.py
+++ Easy/283_moveZeroes.py
@@ -13,22 +13,20 @@
 
 """
 
-
-class Solution():
+class Solution(object):
     def moveZeroes(self, nums):
+        i, j = 0, 0
         for i in range(len(nums)):
-            if nums[i] == 0:
-                for j in range(i+1, len(nums)):
-                    nums[j],nums[j+1] = nums[j+1],nums[j]
-                    j += 1
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
 
-
-
-
+        for j in range(j, len(nums)):
+            nums[j] = 0
 
 if __name__ == "__main__":
     S = Solution()
-    numns = [0, 0, 1]
+    numns = [0, 1, 0, 3, 12]
     print(numns)
     S.moveZeroes(numns)
     print(numns)
