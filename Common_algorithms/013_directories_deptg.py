7f39f3d482b2403a5ceec478516f2cb732175cc9
zhangkun <zhangkun09211916@gmail.com>
2019-03-25 11:27:33 +0800

    List directories with a specified depth in Python

--- Common_algorithms/013_directories_deptg.py
+++ Common_algorithms/013_directories_deptg.py
@@ -1,5 +1,18 @@
 """
     List directories with a specified depth in Python
+
+    import os,string
+    path = '.'
+    path = os.path.normpath(path)
+    res = []
+    for root,dirs,files in os.walk(path, topdown=True):
+        depth = root[len(path) + len(os.path.sep):].count(os.path.sep)
+        if depth == 2:
+            # We're currently two directories in, so all subdirs have depth 3
+            res += [os.path.join(root, d) for d in dirs]
+            dirs[:] = [] # Don't recurse any deeper
+    print(res)
+
 """
 
 import os,string
