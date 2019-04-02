df6fd81175014dcb37a9cbfbb1d383705af4bfb9
zhangkun <zhangkun09211916@gmail.com>
2019-04-02 11:27:45 +0800

    find Max  Average

--- Easy/array/643_findMaxAverage.py
+++ Easy/array/643_findMaxAverage.py
@@ -13,13 +13,44 @@
         所给数据范围 [-10,000，10,000]。
 
 """
-
+"""
 class Solution:
     def findMaxAverage(self, nums, k):
-        pass
+        window = []
+        resMax = None
+        for i, x in enumerate(nums):
+            if i >= k and window[0] <= i - k:
+                window.pop(0)
+            window.append(i)
+            if i >= k - 1:
+                sumMax = 0
+                for j in range(k):
+                    sumMax += nums[window[j]]
+                
+                if resMax is None:
+                    resMax = sumMax
+                if sumMax >= resMax:
+                    resMax = sumMax
+        return resMax / k   
+
+"""
+
+
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        sumMax = sum(nums[0:k])
+        temp = sumMax
+
+        for i in range(k,len(nums)):
+            sumMax = nums[i] + sumMax - nums[i - k]
+            if sumMax > temp:
+                temp = sumMax
+        return temp / k
+
+
 
 if __name__ == "__main__":
     S = Solution()
-    nums = [1,12,-5,-6,50,3]
-    S.findMaxAverage(nums, 4)
+    nums = [-1]
+    print(S.findMaxAverage(nums, 1))
 
