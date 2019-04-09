dc16bf4fea8ded6c1e6a86f10f8321110b6d2200
zhangkun <zhangkun09211916@gmail.com>
2019-04-09 14:54:57 +0800

    isToeplitzMatrix

--- Easy/array/766_isToeplitzMatrix.py
+++ Easy/array/766_isToeplitzMatrix.py
@@ -46,3 +46,19 @@
 
 """
 
+
+class Solution(object):
+    def isToeplitzMatrix(self, matrix):
+        for i in range(len(matrix)-1):
+            for j in  range(len(matrix[0])-1):
+                if matrix[i][j] != matrix[i+1][j+1]:
+                    return False
+        return True
+                #print(matrix[i][j], matrix[i+1][j+1])
+
+if __name__ == "__main__":
+
+    S = Solution()
+    #matrix = [[1,2,3,4],[5,1,2,3],[9,5,1,2]]
+    matrix = [[1, 2],[2, 2]]
+    print(S.isToeplitzMatrix(matrix))
