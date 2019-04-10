869fef8058f129bc7fc1800fd3d414504bd0ca18
zhangkun <zhangkun09211916@gmail.com>
2019-04-10 11:59:12 +0800

    numMagicSquaresInside

--- Easy/array/840_numMagicSquaresInside.py
+++ Easy/array/840_numMagicSquaresInside.py
@@ -38,7 +38,13 @@
 
 class Solution(object):
     def numMagicSquaresInside(self, grid):
-        pass
+        r = 0
+        for i in range(len(grid)-2):
+            for j in range(len(grid[0])-2):
+                if set((grid[i][j],grid[i][j+2],grid[i+2][j],grid[i+2][j+2])) == set((2,4,6,8)) and grid[i+1][j+1] == 5:
+                    if sum(grid[i][j:j+3]) == sum(grid[i+2][j:j+3])==grid[i][j]+grid[i+1][j]+grid[i+2][j] == 15:
+                        r += 1
+        return r
 
 
 
