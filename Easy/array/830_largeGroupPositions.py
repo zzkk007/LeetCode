68a8af873f202b7d154e29dce8602056604194a5
zhangkun <zhangkun09211916@gmail.com>
2019-04-10 11:27:19 +0800

    largeGroupPositions

--- Easy/array/830_largeGroupPositions.py
+++ Easy/array/830_largeGroupPositions.py
@@ -27,14 +27,60 @@
     说明:  1 <= S.length <= 1000
 
 """
+"""
+题意：提取所有符合条件的子串的起始索引和结束索引
 
+条件为：子串每个字符必须相等且连续三个以上
+
+思路：将字符串转为字符数组，遍历整个数组，将符合条件的子串索引添加到List中。
+
+如何筛选符合条件的子串：每次遍历，i代表当前遍历到的位置，开始索引beginIndex指向当前字符，
+结束索引endIndex指向子串末尾索引，times表示出现重复字符的次数。如果符合条件，times自增，
+当times大于3，就记录结束索引endIndex的值。上代码
 
 class Solution(object):
     def largeGroupPositions(self, S):
-        result = []
-        nn = []
-        for i, v in enumerate(S):
-            pass
+
+        resList = list()
+        i, beginIndex = 0, 0
+
+        while i < len(S):
+
+            beginIndex = i
+            times = 0
+            endIndex = -1
+
+            while beginIndex < len(S) and S[i] ==  S[beginIndex]:
+                beginIndex += 1
+                times += 1
+                if times >= 3:
+                    endIndex = i + times - 1
+
+            if endIndex == -1:
+                i += 1
+            else:
+                resList.append([i, endIndex])
+                i = endIndex
+
+        return resList
+"""
+
+# 用双指针遍历，i为始指针，j为尾指针，外层为for循环，用于更新始指针i的值，
+# 内层为while循环（连续的计数通常使用while循环），用于更新尾指针j的值，不断遍历后更新尾指针的值，
+# 如果j-i>=3，就可以添加到list中了，然后在更新始指针i即可。
+
+class Solution(object):
+    def largeGroupPositions(self, S):
+        lists = list()
+        i = 0
+        while i < len(S):
+            j = i
+            while j < len(S) and  S[i] == S[j]:
+                j += 1
+            if j - i >= 3:
+                lists.append([i,j-1])
+            i = j
+        return lists
 
 
 
@@ -45,6 +91,7 @@ class Solution(object):
 if __name__ == "__main__":
 
     S = Solution()
+    #Str = "abcc"
     Str = "abcdddeeeeaabbbcd"
     print(S.largeGroupPositions(Str))
 
