a63048bdac80d9d2c1dc32d5622b605a039f8478
zhangkun <zhangkun09211916@gmail.com>
2019-04-11 11:44:55 +0800

    singleNumber

---
+++ Easy/hashTable/136_singleNumber.py
@@ -0,0 +1,42 @@
+"""
+    136. 只出现一次的数字
+
+    给定一个非空整数数组，除了某个元素只出现一次外，其余每一个元素均出现两次，
+    找出只出现一次的元素。
+
+    说明：
+        你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+    示例 1:
+
+        输入: [2,2,1]
+        输出: 1
+
+    示例 2:
+
+        输入: [4,1,2,1,2]
+        输出: 4
+
+"""
+
+class Solution(object):
+    def singleNumber(self, nums):
+
+        hash_table = dict
+        for i in range(len(nums)):
+            pass
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    S = Solution()
+    nums = [2, 2, 1]
+    print(S.singleNumber(nums))
