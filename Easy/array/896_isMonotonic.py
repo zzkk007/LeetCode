cb08bc6faa6b013047812cfcece7ec04daf645af
zzkk007 <38235797+zzkk007@users.noreply.github.com>
2019-04-13 22:32:28 +0800

    LeetCode

--- Easy/array/896_isMonotonic.py
+++ Easy/array/896_isMonotonic.py
@@ -40,22 +40,47 @@
 
 """
 
-
+"""
 
 class Solution:
     def isMonotonic(self, nums):
-        pass
+
+        list1=sorted(A)
+        list2=sorted(A,reverse=True)
+        if A==list1 or A==list2:
+            return True
+        else:
+            return False
 
 
 
+"""
 
 
 
+class Solution(object):
+
+
+    def isMonotonic(self, nums):
+
+        if A[0] < A[-1]:
+            for i in range(len(A)-1):
+                if A[i] > A[i+1]:
+                    return False
+        elif A[0] > A[-1]:
+            for j in range(len(A)-1):
+                if A[j] < A[j+1]:
+                    return False
+        else:
+            for k in range(len(A)-1):
+                if A[k] != A[k+1]:
+                    return False
+        return True
 
 
 
 if __name__ == "__main__":
-    A = [1, 1, 1, 1]
+    A = [1, 1, 1, 1, 7, 9]
     B = [2, 3]
     S = Solution()
     print(S.isMonotonic(A))
