cb5a6621ce0d6bc7737b703b44af1157416b29a1
zhangkun <zhangkun09211916@gmail.com>
2019-04-18 10:18:07 +0800

    isSameTree

--- Easy/tree/100_isSameTree.py
+++ Easy/tree/100_isSameTree.py
@@ -38,12 +38,23 @@
 """
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+#class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
 
 class Solution(object):
-    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
-        pass
+    def isSameTree(self, p, q):
+        if not p and not q:
+            return  True
+
+        if p is not None and q is not None:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right,q.right)
+            else:
+                return False
+        else:
+            return False
+
+
