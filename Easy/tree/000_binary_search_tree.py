c6d8f148f7081ea37f49774c4da05fed77bb9f38
zhangkun <zhangkun09211916@gmail.com>
2019-04-22 10:28:38 +0800

    leetcode

--- Easy/tree/000_binary_search_tree.py
+++ Easy/tree/000_binary_search_tree.py
@@ -121,5 +121,192 @@ class BinarySearchTree(object):
                 node.parent = None
 
         # 2
+        elif node.left is None and node.right is not None:
+            if node == self.root:
+                self.root = node.right
+                self.root.parent = None
+                node.right = None
+            else:
+
+                if node.val < node.parent.val:
+                    node.parent.left = node.right
+                else:
+                    node.parent.right = node.right
+
+                node.right.parent = node.parent
+                node.parent = None
+                node.right = None
+        elif node.left is not None and node.right is None:
+            if node == self.root:
+                self.root = node.left
+                self.root.parent = None
+                node.left = None
+            else:
+                if node.val < node.parent.val:
+                    node.parent.left = node.left
+                else:
+                    node.parent.right = node.left
+
+                node.left.parent = node.parent
+                node.parent = None
+                node.left = None
+
+        # 3
+        else:
+
+            min_node = node.right
+            # 找到右子树的最小值节点
+
+            if min_node.left:
+                min_node = min_node.left
+
+            if node.val != min_node.val:
+                node.val = min_node.val
+                self._del(min_node)
+
+            #右子树的最小值节点与被删除节点的值相等，再次删除原节点
+            else:
+                self._del(min_node)
+                self._del(node)
+
+    def get_min(self):
+        """
+        返回最小值节点
+        :return:
+        """
+
+        if self.root is None:
+            return None
+
+        n = self.root
+        while n.left:
+            n = n.left
+        return n.val
+
+    def get_max(self):
+        """
+        返回最大值
+        :return:
+        """
+        if self.root is None:
+            return None
+
+        n = self.root
+        while n.right:
+            n = n.right
+        return n.val
+
+    def in_order(self):
+        """
+        中序遍历
+        :return:
+        """
+        if self.root is None:
+            return []
+
+        return self._in_order(self.root)
+
+    def _in_order(self, node):
+        if node is None:
+            return []
+
+        ret = []
+        n = node
+        ret.extend(self._in_order(n.left))
+        ret.append(n.val)
+        ret.extend(self._in_order(n.right))
+
+        return ret
+
+    def __repr__(self):
+        # return str(self.in_order())
+
+        print(str(self.in_order()))
+        return self._draw_tree()
+
+    def _bfs(self):
+        """
+        bfs
+        通过父子关系记录节点编号
+        :return:
+        """
+
+        if self.root is None:
+            return []
+
+        ret = []
+        q = Queue()
+        #队列[节点，编号]
+        q.put((self.root, 1))
+
+        while not q.empty():
+            n = q.get()
+
+            if n[0] is not None:
+                ret.append((n[0].val, n[1]))
+                q.put((n[0].left, n[1]*2))
+                q.put((n[0].right, n[1]*2+1))
+
+        return ret
+
+    def _draw_tree(self):
+        """
+        可视化
+        :return:
+        """
+        nodes = self._bfs()
+        if not nodes:
+            print('This tree has no nodes.')
+            return
+
+        layer_num = int(math.log(nodes[-1][1], 2)) + 1
+
+        prt_nums = []
+
+        for i in range(layer_num):
+            prt_nums.append([None] * 2 ** i)
+
+        for v, p in nodes:
+            row = int(math.log(p, 2))
+            col = p % 2 ** row
+            prt_nums[row][col] = v
+
+        prt_str = ''
+        for l in prt_nums:
+            prt_str += str(l)[1:-1] + '\n'
+
+        return prt_str
+
+
+if __name__ == "__main__":
+    nums = [4, 2, 5, 6, 1, 7, 3]
+    bst = BinarySearchTree(nums)
+
+    print(bst)
+
+    # 插入
+    bst.insert(1)
+    bst.insert(4)
+    print(bst)
+
+    # 搜索
+    for n in bst.search(2):
+        print(n.parent.val, n.val)
+
+    # 删除
+    bst.insert(6)
+    bst.insert(7)
+    print(bst)
+    bst.delete(7)
+    print(bst)
+    bst.delete(6)
+    print(bst)
+    bst.delete(4)
+    print(bst)
+
+    # min max
+    print(bst.get_max())
+    print(bst.get_min())
+
 
 
