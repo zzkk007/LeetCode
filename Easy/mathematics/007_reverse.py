4e7f6242a37cc5bd889fda93eb9103389ace07b9
zzkk007 <38235797+zzkk007@users.noreply.github.com>
2019-05-07 21:05:44 +0800

    reverse

--- Easy/mathematics/007_reverse.py
+++ Easy/mathematics/007_reverse.py
@@ -26,9 +26,22 @@
 
 
 class Solution(object):
-    def reverse(self, x: int) -> int:
-        return 12
+    def reverse(self, x: int):
 
+        if x == 0:
+            return 0
+        str_x = str(x)
+        x = ''
+        if str_x[0] == "-":
+            x += '-'
+
+        x += str_x[len(str_x)-1::-1].lstrip("0").rstrip("-")
+
+        x = int(x)
+        if -1**31 < x < 2**31-1:
+            return x
+
+        return 0
 
 
 if __name__ == "__main__":
