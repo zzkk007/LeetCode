9a27dde3d44a88f7fab7cab8caf8befc8a77e779
zhangkun <zhangkun09211916@gmail.com>
2019-05-08 19:53:55 +0800

    romanToInt

--- Easy/string/013_romanToInt.py
+++ Easy/string/013_romanToInt.py
@@ -50,27 +50,21 @@
 
 """
 
+
+
+
 class Solution:
-    def romanToInt(self, s: str) -> int:
-
-        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
-                 'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
-
-
-        sumNum = 0
-        i = 0
-        num = len(s) - 1
-        while num:
-            if roman[s[i]] < roman[s[i+1]]:
-                sumNum +=  roman[s[i] + s[i+1]]
-                num -= 2
-                i += 2
+    def romanToInt(self, s: str):
+        a = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        ans=0
+        for i in range(len(s)):
+            if i<len(s)-1 and a[s[i]]<a[s[i+1]]:
+                ans-=a[s[i]]
             else:
-                sumNum += roman[s[i]]
-                i += 1
-                num -= 1
+                ans+=a[s[i]]
+        return ans
 
-        return sumNum
+
 
 if __name__ == "__main__":
     S = Solution()
