2603d692228fd29ebfacb863c374321157e59e35
zhangkun <zhangkun09211916@gmail.com>
2019-05-08 19:41:17 +0800

    romanToInt

--- Easy/string/013_romanToInt.py
+++ Easy/string/013_romanToInt.py
@@ -49,3 +49,29 @@
     解释: M = 1000, CM = 900, XC = 90, IV = 4.
 
 """
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+
+        roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000,
+                 'IV':4, 'IX':9, 'XL':40, 'XC':90, 'CD':400, 'CM':900}
+
+
+        sumNum = 0
+        i = 0
+        num = len(s) - 1
+        while num:
+            if roman[s[i]] < roman[s[i+1]]:
+                sumNum +=  roman[s[i] + s[i+1]]
+                num -= 2
+                i += 2
+            else:
+                sumNum += roman[s[i]]
+                i += 1
+                num -= 1
+
+        return sumNum
+
+if __name__ == "__main__":
+    S = Solution()
+    print(S.romanToInt("IVII"))
