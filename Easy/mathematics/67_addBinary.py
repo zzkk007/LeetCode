b4c2f8a94166565d4027359ea27e77ba29e168ae
zhangkun <zhangkun09211916@gmail.com>
2019-05-10 21:28:06 +0800

    addBinary

--- Easy/mathematics/67_addBinary.py
+++ Easy/mathematics/67_addBinary.py
@@ -1,8 +1,6 @@
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        decimala = int(a, 2)
-        decimalb = int(b, 2)
 
         return bin(int(a,2) + int(b, 2)).replace('0b','')
 
